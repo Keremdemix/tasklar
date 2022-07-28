0450ab4f3225826f894d5011016c95709ce084b6
Keremdemix <keremdemix@outlook.com>
2022-07-28 16:05:20 +0300

    adding parse task files

--- task_csv_sol.py
+++ task_csv_sol.py
@@ -1,7 +1,3 @@
-import re
-
-
-
 csv_line = '2013-03-15 15:59:21,FHY,FG224B3907501199,N/A,192.168.1.133,52906,173.194.39.154,80,root,11257,http,googlesyndication.com,62551133,URL has been visited,webfilter'
 
 key_value_line = 'date=2013-03-15 15:59:21,esname=FHY,esid=FG224B3907501199,es_group=N/A,src=192.168.1.133,spt=52906,dst=173.194.39.154,dpt=80,vdom=root,rcv=11257,proto=http,url=googlesyndication.com,sid=62551133,info=URL has been visited,cat=webfilter'
@@ -10,94 +6,83 @@ js_string_line = '{"BytesReceived": 11257, "DestinationIP": "173.194.39.154", "D
 
 
 parsed_result = {
-  'Bytes': {
-    'Received': '11257'
-  },
-  'Destination': {
-    'IP': '173.194.39.154',
-    'Port': '80'
-  },
-  'Event': {
-    'Category': 'webfilter',
-    'Info': 'URL has been visited'
-  },
-  'EventSource': {
-    'ID': 'FG224B3907501199',
-    'Name': 'FHY',
-    'Group':'N/A'  
-  },
-  'Policy': {
-    'ID': '86'
-  },
-  'Service': {
-    'Name': 'http'
-  },
-  'Session': {
-    'ID': '62551133'
-  },
-  'Source': {
-    'IP': '192.168.1.133',
-    'Port': '52906',
-    'UserName': 'HULYA.OZASLAN'
-  },
-  'Time': {
-    'Generated': '2013-03-15 15:59:21'
-  },
-  'URL': {
-    'BaseDomain': 'googlesyndication.com'
-  },
-  'Virtual': {
-    'Domain': 'root'
-  },
-  'DataType': {
-    'log'
-  }
-}
+    'Bytes': {
+        'Received': 11257
+    },
+    'Destination': {
+        'IP': '173.194.39.154',
+        'Port': '80'
+    },
+    'Event': {
+        'Category': 'webfilter',
+        'Info': 'URL has been visited'
+    },
+    'EventSource': {
+        'ID': 'FG224B3907501199',
+        'Name': 'FHY',
+        'Group': 'N/A'
+    },
+    'Policy': {
+        'ID': '86'
+    },
+    'Service': {
+        'Name': 'http'
+    },
+    'Session': {
+        'ID': '62551133'
+    },
+    'Source': {
+        'IP': '192.168.1.133',
+        'Port': '52906',
+        'UserName': 'HULYA.OZASLAN'
+    },
+    'Time': {
+        'Generated': '2013-03-15 15:59:21'
+    },
+    'URL': {
+        'BaseDomain': 'googlesyndication.com'
+    },
+    'Virtual': {
+        'Domain': 'root',
+        'DataType': 'log'
+    }}
 
 
-
-
-parsed_result2={
-  'Bytes':{
-    'Received':re.findall('\d+',csv_line)[18]
-  },
-  'Destination':{
-    'IP':re.findall('\d{3}\D\d{3}\D\d{2}\D\d{3}',csv_line)[0],
-    'Port':re.findall('\d{2}',csv_line)[22]
-  },
-  'Event':{
-    'Category':re.findall(r'N/A|\w+' ,csv_line)[29],
-    'Info':re.findall('URL has\D+ed',csv_line)[0]
-  },
-  'EventSource':{
-    'ID':re.findall('\w{16}', csv_line)[0],
-    'Name':re.findall('[A-Z]{3}',csv_line)[0],
-    'Group':re.findall(r'N/A|\w+',csv_line)[8]
-  },
-  'Policy': {
-    'ID': '86'
-  },
-  'Service':{
-    'Name':re.findall('\w+',csv_line)[22]
-  },
-  'Session':{
-    'ID':re.findall('\d{8}',csv_line)[1]
-  },
-  'Source': {
-    'IP':re.findall('\d{3}\D\d{3}\D\d\D\d{3}',csv_line)[0] ,
-    'Port':re.findall('(6553[0-5]|655[0-2]\d|65[0-4]\d\d|6[0-4]\d{3}|5\d{4}|49[2-9]\d\d|491[6-9]\d|4915[2-9])',csv_line)[1] ,
-    'UserName':'HULYA.OZASLAN'
-  },
-  'Time': {
-    'Generated': re.findall('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}',csv_line)[0]
-  },
-  'URL': {
-    'BaseDomain':re.findall('\w+\.\w+' ,csv_line)[4]
-  },
-  'Virtual': {
-    'Domain':re.findall('\w+',csv_line)[20]
-  },
-   'DataType': {
-    'log'
-  }
-} 
+def parse_func(text):
+    csv_list = text.split(",")
+    global parsed_results
+    parsed_results = {
+        'Bytes': {
+            'Received': csv_list[9]
+        },
+        'Destination': {
+            'IP': csv_list[6], 'Port': csv_list[7]
+        },
+        'Event': {
+            'Category': csv_list[14],  'Info': csv_list[13]
+        },
+        'EventSource': {
+            'ID': csv_list[2], 'Name': csv_list[1], 'Group': csv_list[3]
+        },
+        'Policy': {
+            'ID': '86'
+        },
+        'Service': {
+            'Name': csv_list[10]
+        },
+        'Session': {
+            'ID': csv_list[12]
+        },
+        'Source': {
+            'IP': csv_list[4], 'Port': csv_list[5], 'UserName': 'HULYA.OZASLAN'
+        },
+        'Time': {
+            'Generated': csv_list[0]
+        },
+        'URL': {
+            'BaseDomain': csv_list[11]
+        },
+        'Virtual': {
+            'Domain': csv_list[8], 'DataType': 'log'
+        }}
+    return parsed_results
--- test_task_csv_sol.py
+++ test_task_csv_sol.py
@@ -1,46 +1,46 @@
 import unittest
 import task_csv_sol
 
-
-
 class Test_csv_match (unittest.TestCase):
-    
-  def test_csv_bytes_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Bytes'],task_csv_sol.parsed_result['Bytes'])
+    def test_csv_parsed_results(self):
+        task_csv_sol.parse_func(task_csv_sol.csv_line)
+        self.assertEqual(
+            task_csv_sol.parsed_results['Bytes']['Received'], '11257')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Destination']['Port'], '80')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Destination']['IP'], '173.194.39.154')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Event']['Category'], 'webfilter')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Event']['Info'], 'URL has been visited')
+        self.assertEqual(
+            task_csv_sol.parsed_results['EventSource']['ID'], 'FG224B3907501199')
+        self.assertEqual(
+            task_csv_sol.parsed_results['EventSource']['Name'], 'FHY')
+        self.assertEqual(
+            task_csv_sol.parsed_results['EventSource']['Group'], 'N/A')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Policy']['ID'], '86')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Service']['Name'], 'http')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Session']['ID'], '62551133')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Source']['IP'], '192.168.1.133')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Source']['Port'], '52906')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Source']['UserName'], 'HULYA.OZASLAN')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Time']['Generated'], '2013-03-15 15:59:21')
+        self.assertEqual(
+            task_csv_sol.parsed_results['URL']['BaseDomain'], 'googlesyndication.com')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Virtual']['Domain'], 'root')
+        self.assertEqual(
+            task_csv_sol.parsed_results['Virtual']['DataType'], 'log')
 
-  def test_csv_Destination_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Destination'],task_csv_sol.parsed_result['Destination'])
 
-  def test_csv_Event_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Event'],task_csv_sol.parsed_result['Event'])
-  
-  def test_csv_EventSource_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['EventSource'],task_csv_sol.parsed_result['EventSource'])
-
-  def test_csv_Policy_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Policy'],task_csv_sol.parsed_result['Policy'])
-
-  def test_csv_Service_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Service'],task_csv_sol.parsed_result['Service'])
-
-  def test_csv_Session_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Session'],task_csv_sol.parsed_result['Session'])
-
-  def test_csv_Source_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Source'],task_csv_sol.parsed_result['Source'])
-
-  def test_csv_Time_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Time'],task_csv_sol.parsed_result['Time'])
-
-  def test_csv_URL_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['URL'],task_csv_sol.parsed_result['URL'])
-
-  def test_csv_Virtual_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['Virtual'],task_csv_sol.parsed_result['Virtual'])
-
-  def test_csv_DataType_match (self):
-    self.assertEqual(task_csv_sol.parsed_result2['DataType'],task_csv_sol.parsed_result['DataType'])
-
-
-if __name__=='__main__':
-  unittest.main()
+if __name__ == '__main__':
+    unittest.main()
