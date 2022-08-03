45d35b1fa06d16d513a37718d69e552d535a895f
Keremdemix <keremdemix@outlook.com>
2022-08-03 14:52:11 +0300

    adding excel task files

---
+++ excel_task.py
@@ -0,0 +1,26 @@
+import excel2json
+import json
+data=excel2json.convert_from_file('data.xlsx')
+x={}
+with open('Sayfa1.json') as json_file:
+
+    data = json.load(json_file)
+
+    for i in range (len(data)):
+        
+       key=data[i]["VendorID"]
+       
+       x[str(int(key))]={ 
+        
+            "Context": data[i]["EventMap.Context"],
+            "SubType":  data[i]["EventMap.SubType"],
+            "Type":  data[i]["EventMap.Type"] 
+        
+        }
+    
+    dest = dict(x)  # or orig.copy()
+    jsonString = json.dumps(dest)
+    jsonFile = open(("data.json"), "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+      
